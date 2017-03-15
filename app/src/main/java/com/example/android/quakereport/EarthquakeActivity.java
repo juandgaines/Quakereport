48281e98c70bfc58dab99f7730f1bd5c30e59be1
juandgaines <jdavid90.2@gmail.com>
2017-03-15 00:44:49 -0500

    Now app works with AsyncTask, but we can improve memory managment due rotation of device using Loaders and the equivalent AsynTaskLoader.

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -17,6 +17,7 @@ package com.example.android.quakereport;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -28,6 +29,9 @@ import java.util.ArrayList;
 public class EarthquakeActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL="https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&eventtype=earthquake&orderby=time&minmag=5&limit=10";
+    private ArrayList<Earthquake> earthquakes= new ArrayList<>();
+    private EarthquakeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,10 @@ public class EarthquakeActivity extends AppCompatActivity {
         setContentView(R.layout.earthquake_activity);
 
         // Create a fake list of earthquake locations.
-        ArrayList<Earthquake> earthquakes=QueryUtils.extractEarthquakes();
+      adapter = new EarthquakeAdapter(
+                this, R.layout.list_item, earthquakes);
         /*
+
         earthquakes.add(new Earthquake("7.2" ,"San Francisco", "Feb 2, 2016"));
         earthquakes.add(new Earthquake("6.1" ,"London","July 20, 2015"));
         earthquakes.add(new Earthquake("3.9" ,"Tokyo","Nov 10, 2014"));
@@ -45,11 +51,13 @@ public class EarthquakeActivity extends AppCompatActivity {
         earthquakes.add(new Earthquake("4.9" ,"Rio de Janeiro", "Aug 19, 2012"));
         earthquakes.add(new Earthquake("1.6", "Paris","Oct 30, 2011"));*/
 
-
+        EarthquakeAsyncTask mTaskMain= new EarthquakeAsyncTask();
+        mTaskMain.execute(USGS_REQUEST_URL);
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
-        final EarthquakeAdapter adapter = new EarthquakeAdapter(
-                this, R.layout.list_item, earthquakes);
+
+
+
         earthquakeListView.setAdapter(adapter);
 
         earthquakeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -63,11 +71,43 @@ public class EarthquakeActivity extends AppCompatActivity {
             }
         });
 
+
         // Create a new {@link ArrayAdapter} of earthquakes
 
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
 
+    }
+    private class EarthquakeAsyncTask extends AsyncTask<String,Void,ArrayList<Earthquake>>{
+
+
+        @Override
+        protected ArrayList<Earthquake> doInBackground(String... urls) {
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+            ArrayList<Earthquake> mEvent= QueryUtils.fetchEarthquakeData(urls[0]);
+            return mEvent;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Earthquake> event) {
+            super.onPostExecute(event);
+            adapter.clear();
+            if (event== null || event.isEmpty()) {
+                return;
+            }
+            else{
+                earthquakes.addAll(event);
+            }
+
+            adapter.notifyDataSetChanged();
+
+
+
+
+
+        }
     }
 }
